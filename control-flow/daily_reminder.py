fc9c2e649b7227b734aabe3fb36a7c877def0368
codebyemma <mysubbacc@gmail.com>
2025-09-18 16:50:12 +0100

    very petty checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,22 @@
-task = input("Enter your task: ")
-priority = input("Priority (low, medium, high): ")
-time-bound = input("Is it timr-bound? (yes/no): ")
+Task = input("Enter your task: ")
+Priority = input("Priority (low, medium, high): ")
+Time-Bound = input("Is it timr-bound? (yes/no): ")
 
 match pirority:
     case "low":
-        if time-bound.lower() == "yes":
-            print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+        if Time-Bound == "yes":
+            print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
         else:
-            print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
     case "medium":
-        if time-bound.lower() == "yes":
-            print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+        if Iime-Bound == "yes":
+            print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
         else:
-            print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
     case "high":
-        if time-bound.lower() == "yes":
-            print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+        if Time-Bound == "yes":
+            print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
         else:
-            print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
     case _:
         print("Invalid priority level.")
