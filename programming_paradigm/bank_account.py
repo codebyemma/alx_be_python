1be9c01535f268f19f07a674e760e389c3169400
codebyemma <mysubbacc@gmail.com>
2025-10-05 00:16:26 +0100

    fhhfi

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        return self.account_balance
+        return f"Current Balance: {self.account_balance}"
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
         den = float(denominator)
         if den == 0:
             return "Error: Cannot divide by zero."
-        return num / den
+        return f"The result of the division is {num / den}"
     except ValueError:
         return("Error: Please enter numeric values only.")
     
