74f04ef635fa270e8094fc244450a094bab23a62
codebyemma <mysubbacc@gmail.com>
2025-10-05 00:26:58 +0100

    fhhfi

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -23,7 +23,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(0, 5), 0)
         
-    def test_divide(self):
+    def test_division(self):
         """Test the division method."""
         self.assertEqual(self.calc.divide(6, 2), 3)
         self.assertEqual(self.calc.divide(0, 1), 0)
