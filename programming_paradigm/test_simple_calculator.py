54eaf3ec930662c7dc3a54f47d616fed54754aad
codebyemma <mysubbacc@gmail.com>
2025-10-05 00:22:49 +0100

    fhhfi

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,11 +11,12 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
-    def test_subtract(self):
+
+    def test_subtraction(self):
         """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 1), -1)
-        self.assertEqual(self.calc.subtract(2, 1), 1)
+        self.assertEqual(self.calc.subtraction(2, 1), 1)
         
     def test_multiply(self):
         """Test the multiplication method."""
